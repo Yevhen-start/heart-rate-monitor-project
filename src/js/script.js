e9a3cfde138189f8c834bf26b3f4305aca5d55db
Евгений Хрептун <khreptunchik@gmail.com>
2022-04-04 20:22:21 +0300

    added a section containing tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    // JQwery для слайдера
     $('.carousel__inner').slick({
         speed: 1200,
         // adaptiveHeight: true,
@@ -14,8 +15,32 @@ $(document).ready(function(){
             }        
         ]
       });
+      // JQwery для табов
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab--active)', function() {
+        $(this)
+          .addClass('catalog__tab--active').siblings().removeClass('catalog__tab--active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content--active').eq($(this).index()).addClass('catalog__content--active');
+      });
+      // JQwery для клика
+
+      function toggleSlide(item) {
+        $(item).each(function(i) {
+          $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content--active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list--active');
+          });
+        });
+      };
+      
+      toggleSlide('.catalog-item__link');
+      toggleSlide('.catalog-item__back');
+
   });
 
+
+
+  // JS слайдер Swiper
 const slides = document.querySelector('.swiper')
 
 const mySwiper = new Swiper(slides, {
