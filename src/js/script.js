9674b368f31d55b26fc84964997c218e38bacebc
Евгений Хрептун <khreptunchik@gmail.com>
2022-04-09 19:03:35 +0300

    added footer

--- src/js/script.js
+++ src/js/script.js
@@ -36,6 +36,63 @@ $(document).ready(function(){
       toggleSlide('.catalog-item__link');
       toggleSlide('.catalog-item__back');
 
+      // Модальные окна
+      $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+      });
+      $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+      });
+      $('.button--mini').each(function(i) {
+        $(this).on('click', function(){
+          $('#order .modal__description').text($('.catalog-item__subtitle').eq(i).text());
+          $('.overlay, #order').fadeIn('slow');
+        });  
+      });
+      // Валидация
+
+      function validateForm (form) {
+        $(form).validate({
+          rules: {
+            name: "required",
+            phone: "required",
+            email: {
+              required: true,
+              email: true
+            }
+            },
+            messages: {
+              name: "Пожалуйста, введите свое имя",
+              phone: "Пожалуйста, введите свой номер телефон",
+              email: {
+                required: "Пожалуйста, введите свою почту",
+                email: "Неправильно введен адрес почты" 
+              }
+          }
+        }); 
+      };
+      validateForm('#consultation-form form');
+      validateForm('#consultation form');
+      validateForm('#order form');
+
+      // Маска
+
+      $('input[name=phone]').mask('+3(999) 999-99-99');
+
+      // Скрол вверх
+      $(window).scroll(function(){
+        if ($(this).scrollTop() > 800) {
+          $('.pageup').fadeIn();
+        } else {
+          $('.pageup').fadeOut();
+        }
+      });
+
+      $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+      });
   });
 
 
